0bc6874d0b068ef1c8530be445097f7b7732b4ef
Steve Dekorte <steve@dekorte.com>
2019-05-21 10:32:13 -0700

    moving recognizer to three.js-dev

---
+++ recognizer/Object3D-extras.js
@@ -0,0 +1,412 @@
+
+THREE.Vector3.prototype.roughlyEquals = function(v2) {
+    const v1 = this;
+    const d = 0.001;
+    const dx = Math.abs(v1.x - v2.x);
+    const dy = Math.abs(v1.y - v2.y);
+    const dz = Math.abs(v1.z - v2.z);
+    return (dx < d) && (dy < d) && (dz < d);  
+}
+
+const Obj3d = THREE.Object3D.prototype
+
+Obj3d.makeDoubleSided = function() {
+    //this.material.side = THREE.DoubleSide;
+
+    this.traverse( function(node) { 
+        if (node instanceof THREE.Mesh) { 
+            node.material.side = THREE.DoubleSide;
+        } 
+    } );
+}
+
+Obj3d.makeCastShadow = function() {
+    this.castShadow = true;
+    //this.receiveShadow = true;
+    
+    this.traverse((node) => { 
+        if (node instanceof THREE.Mesh ) { 
+            node.castShadow = true; 
+            //node.receiveShadow = true; 
+        } 
+    })
+}
+
+
+function LinePieces_hasSharedVerts(vertices, v1, v2, ignoreIndex) { 
+    // assumes all vertices are pairs of lines and not shared
+    for (let i = 0; i < vertices.length; i += 2) {
+        if (i === ignoreIndex) {
+            continue;
+        }
+            
+        const l1 = vertices[i]
+        const l2 = vertices[i+1]
+        
+        // see if the vertices match
+        if (l1.roughlyEquals(v1) && l2.roughlyEquals(v2)) {
+            // if their normals match, this isn't an edge
+            if (v1.face.normal.roughlyEquals(l1.face.normal)) {
+                return true
+            }
+        }
+        
+        // see if the vertices match in the other direction
+        if (l2.roughlyEquals(v1) && l1.roughlyEquals(v2)) {
+            // if their normals match, this isn't an edge
+            if (v1.face.normal.roughlyEquals(l1.face.normal)) {
+                return true
+            }
+        }
+    }
+    
+    return false
+}
+        
+Obj3d.firstGeometry = function() {
+/*
+    this.traverse(function(node) { 
+        //if (node instanceof THREE.Mesh) { 
+            if (node.geometry) {
+                return node.geometry
+            }
+        //}
+    })
+*/
+    
+    if (this.geometry) {
+        return this.geometry
+    }
+    for (let i = 0; i < this.children.length; i++) {
+        const child = this.children[i]
+        const g = child.firstGeometry()
+        if (g) {
+            return g
+        }
+    }
+    
+   console.log("unable to find a geometry for this Object3D:", this)
+    throw new Error("unable to find a geometry for this Object3D:", this)
+    return null
+}
+
+Obj3d.calculateRadius = function() {    
+    const geometry = this.firstGeometry(); 
+    geometry.computeBoundingBox(); 
+    const boundingBox = geometry.boundingBox.clone();
+    
+    /*
+    console.log('bounding box coordinates: ' + 
+        '(' + boundingBox.min.x + ', ' + boundingBox.min.y + ', ' + boundingBox.min.z + '), ' + 
+        '(' + boundingBox.max.x + ', ' + boundingBox.max.y + ', ' + boundingBox.max.z + ')' );
+    */
+    
+    const dx = (boundingBox.max.x - boundingBox.min.x);
+    const dy = (boundingBox.max.y - boundingBox.min.y);
+    const dz = (boundingBox.max.z - boundingBox.min.z);
+
+        //this._radius = boundingBox.getBoundingSphere().radius
+    const r = Math.sqrt(dx*dx + dy*dy + dz*dz)
+    console.log("calculated radius = ", r)
+    return r
+}
+
+Obj3d.calculateCenterPoint = function() {
+    const geometry = this.firstGeometry(); 
+    geometry.computeBoundingBox(); 
+    const boundingBox = geometry.boundingBox.clone();
+    
+    const dx = (boundingBox.max.x - boundingBox.min.x);
+    const dy = (boundingBox.max.y - boundingBox.min.y);
+    const dz = (boundingBox.max.z - boundingBox.min.z);
+
+    const cx = boundingBox.min.x + dx/2;
+    const cy = boundingBox.min.y + dy/2;
+    const cz = boundingBox.min.z + dz/2;
+    return new THREE.Vector3(cx, cy, cz)
+}
+
+Obj3d.centerPoint = function() {
+    if (!this._centerPoint) {
+        this._centerPoint = this.calculateCenterPoint()
+    }
+    return this._centerPoint
+}
+
+Obj3d.moveToOrigin = function() {
+    console.log("this.position = ", this.position)
+    this.position = this.position.add(this.calculateCenterPoint().negate())
+    console.log("this.position = ", this.position)
+}
+
+Obj3d.scaleToUnit = function() {
+    this.scale = this.scale.multiplyScalar(1/this.radius())
+}
+
+Obj3d.radius = function() { 
+    if (!this._radius) {
+        this._radius = this.calculateRadius()
+    }     
+    return this._radius
+}
+
+Obj3d.addOutline = function(color, thickness, opacity) {   
+    if (color === undefined) { color = 0xff6600; }
+    if (thickness === undefined) { thickness = 10; }
+    if (opacity === undefined) { opacity = 1; }
+
+    //const outline = this.asLineObject(color, thickness, opacity)
+    const outline = this.asEdgesObject(color, thickness, opacity)
+    outline.recursiveSetColor(color)
+    const s = 1.004
+    outline.scale.set(s, s, s)
+    this.add(outline);
+    return outline
+}
+
+Obj3d.asEdgesObject = function(color, thickness, opacity) {   
+    const newMat = new THREE.LineBasicMaterial({
+        color: color, 
+        linewidth: thickness,
+        fog: true,
+        transparent: opacity != 1,
+        opacity: opacity,
+        linecap: "round",
+        linejoin: "round",
+    });
+
+    const group = new THREE.Object3D()
+
+    this.traverse(function(node) { 
+        if (node instanceof THREE.Mesh) { 
+            const edges = new THREE.EdgesGeometry( node.geometry );
+            const line = new THREE.LineSegments( edges, newMat );
+            group.add(line)
+        }
+    })
+
+    return group
+}
+
+
+Obj3d.asLineObject = function(color, thickness, opacity) {   
+    const newMat = new THREE.LineBasicMaterial( 
+        {
+            color: color, 
+            //color: 0xff5500, 
+            //color: 0x0055ff, 
+            linewidth: thickness,
+            fog: true,
+            transparent: opacity != 1,
+            opacity:opacity,
+            linecap: "round",
+            linejoin: "round",
+        }
+    );
+    
+    /*
+    const sharedMat = new THREE.LineBasicMaterial( 
+        {
+            color: 0x0000ff, 
+            linewidth: 4,
+            fog: true,
+            transparent: true,
+            opacity:.5,
+        }
+    );
+    */
+    
+    /*
+    const normalMat = new THREE.LineBasicMaterial( 
+        {
+            color: 0xffff00, 
+            linewidth: 6,
+            fog: true,
+            transparent: true,
+            opacity: 1,
+        }
+    );
+    */
+
+    const newGeo = new THREE.Geometry();
+    const newVerts = newGeo.vertices
+    
+    const sharedGeo = new THREE.Geometry();
+    const sharedVerts = sharedGeo.vertices
+    
+    //const normalGeo = new THREE.Geometry();
+    //const normals = normalGeo.vertices
+        
+    const allVerts = []
+
+    this.traverse(function(node) { 
+        if (node instanceof THREE.Mesh) { 
+            const faces = node.geometry.faces
+            
+            node.geometry.computeFaceNormals()
+                                    
+            const verts = node.geometry.vertices
+            
+            for (let i = 0; i < faces.length; i ++) {
+                let f = faces[i]
+                
+                const a = new THREE.Vector3().copy(verts[f.a])
+                const b = new THREE.Vector3().copy(verts[f.b])
+                const c = new THREE.Vector3().copy(verts[f.c])
+                /*                            
+                const a = verts[f.a].clone()
+                const b = verts[f.b].clone()
+                const c = verts[f.c].clone()
+                */                                                     
+                a.face = f
+                b.face = f
+                c.face = f
+                
+                allVerts.push(a)
+                allVerts.push(b)
+
+                allVerts.push(b)
+                allVerts.push(c)
+            
+                allVerts.push(c)
+                allVerts.push(a)
+
+                /*
+                const ns = 5
+                const n = f.normal.clone().multiplyScalar(ns)
+                normals.push(a)
+                normals.push(a.clone().add(n))
+                
+                normals.push(b)
+                normals.push(b.clone().add(n))
+                
+                normals.push(c)
+                normals.push(c.clone().add(n))
+                */
+            }
+        } 
+    });
+            
+    console.log("allVerts = ", allVerts.length)
+    console.log("full line count assuming no shared vertices = ", allVerts.length/2)
+    console.log("now we need to trim to only the lines we want to be visible")
+    
+    // this code assumes the vertices are arranged like?
+    // [traingle1PointA, traingle1PointB, traingle1PointC, traingle2PointA, traingle2PointB, ...]
+    // That is, no shared vertices?
+
+    for (let i = 0; i < allVerts.length; i += 2) {           
+        const a = allVerts[i]
+        const b = allVerts[i+1]
+
+        /*
+        console.log("line ", i/2)
+        console.log("  a ", a)
+        console.log("  b ", b)
+        console.log("  a.norm ", a.face.normal)
+        */
+
+        if (LinePieces_hasSharedVerts(allVerts, a, b, i)) {
+            sharedVerts.push(a)
+            sharedVerts.push(b)  
+        } else {
+            newVerts.push(a)
+            newVerts.push(b)
+            
+            /*
+            const ns = -10
+            let n = a.face.normal.clone().multiplyScalar(ns)
+            normals.push(a)
+            normals.push(a.clone().sub(n))
+            
+            n = b.face.normal.clone().multiplyScalar(ns)
+            normals.push(b)
+            normals.push(b.clone().sub(n))
+            */
+        }
+    } 
+    
+    const newObj = new THREE.Line( newGeo, newMat, THREE.LinePieces );
+    //const out = new THREE.Object3D();
+    //out.add(newObj)
+                
+    // console.log("newVerts = ", newVerts.length)
+    //console.log("sharedVerts = ", sharedVerts.length)
+    //out.add(new THREE.Line(sharedGeo, sharedMat, THREE.LinePieces ))
+    //out.add(new THREE.Line(normalGeo, normalMat, THREE.LinePieces ))
+    
+    //newObj.position.copy(this.position)
+    //newObj.scale.copy(this.scale)
+    //newObj.rotation.copy(this.rotation)
+
+
+
+    return newObj
+}
+
+Obj3d.recursiveSetLineWidth = function(width) {           
+    this.traverse( (node) => { 
+        if ( node instanceof THREE.Mesh ) { 
+            node.material.linewidth =  4
+            node.needsUpdate = true
+        } 
+    } );
+}
+
+/*
+Obj3d.randomizePos = function() {
+    this.position.x = 2000 * (Math.random() - 0.5)
+    this.position.y = 2000 * (Math.random() - 0.0) + 5
+    this.position.z = 1000 * (Math.random() - 0.5)    
+}
+*/
+
+Obj3d.recursiveSetOpacity = function(opacity) {           
+    this.traverse((node) => { 
+        if (node instanceof THREE.Mesh) { 
+            if (opacity == 1) {
+                node.material.transparent = false
+            } else {
+                node.material.transparent = true
+                node.material.opacity = opacity
+            }
+
+            node.needsUpdate = true
+        } 
+    } );
+}
+
+Obj3d.recursiveSetColor = function(hexColor) {           
+    this.traverse( (node) => { 
+        if ( node instanceof THREE.Mesh ) { 
+            //console.log("----")
+            //console.log("node:", typeof(node), JSON.stringify(Object.keys(node)))
+            //console.log("----")
+            //console.log("node.material:", typeof(node.material), node.material.length)
+            //console.log("----")
+            //console.log("node.material.color:", typeof(node.material.color), node.material.color)
+            //console.log("----")
+            //if (typeof(node.material) === "Array") {
+                for (let i = 0; i < node.material.length; i++) {
+                    let m = node.material[i]
+                    m.color.setHex(hexColor)
+                }
+            //}
+            /*
+            if (node.material.color) {
+                node.material.color.setHex(hexColor)
+                node.needsUpdate = true
+                console.log("set color")
+            }
+            */
+
+            /*
+            if (node.material.ambient)
+            {
+                node.material.ambient.setHex(hexColor)
+            }
+            */
+        } 
+    } );
+}
+
--- recognizer/main.js
+++ recognizer/main.js
@@ -1,4 +1,6 @@
 
+
+
 if (!String.prototype.capitalized) {
     String.prototype.capitalized = function () {
         return this.replace(/\b[a-z]/g, function (match) {
@@ -65,6 +67,59 @@ class BaseObject {
 
   // ----------------------------------------------------------------------------------
 
+  class PuzzlePiece extends BaseObject {
+      // this class puts several blocks next to each other and then adds
+      // a edges wireframe around it
+    init() {
+        
+    
+
+    }
+  }
+
+
+  /*
+
+  class JSScript extends ResourceLoaderBase {
+    init() {
+        super.init()
+        this.newSlot("importer", null);
+        this.newSlot("fullPath", null);
+        this.newSlot("doneCallback", null);
+    }
+
+    run () {
+        const script = document.createElement("script")
+
+        //console.log("JSScript loading: '" + this.fullPath() + "'")
+
+        script.src = this.fullPath()
+
+        script.onload = () => {
+            //console.log("loaded script src:'" + script.src + "' type:'" + script.type + "' text:[[[" + script.text + "]]]")
+            this._doneCallback()
+        }
+
+        script.onerror = (error) => {
+            this.importer().setError(error)
+            throw new Error("missing url " + this.fullPath())
+        }
+
+        const parent = document.getElementsByTagName("head")[0] || document.body
+        parent.appendChild(script)
+    }
+
+    basePath () {
+        const parts = this.fullPath().split("/")
+        parts.pop()
+        const basePath = parts.join("/")
+        return basePath
+    }
+}
+*/
+
+  // ----------------------------------------------------------------------------------
+
 class App extends BaseObject {
     init() {
         super.init()
@@ -80,7 +135,12 @@ class App extends BaseObject {
         this.newSlot("mainObject", null);
         this.newSlot("spotlight", null);
 
+        
+        //THREE.EventDispatcher.prototype.apply( THREE.OBJMTLLoader.prototype ); 
+
         this.setup()
+
+
     }
 
     newSpotlight() {
@@ -89,8 +149,8 @@ class App extends BaseObject {
         light.castShadow = true;
 
         const detail = 4
-        light.shadowMapWidth  = 512 * detail;
-        light.shadowMapHeight = 512 * detail;
+        light.shadow.mapSize.width  = 512 * detail;
+        light.shadow.mapSize.height  = 512 * detail;
         //light.shadowCameraVisible = true;
         return light       
     }
@@ -99,7 +159,7 @@ class App extends BaseObject {
 
         this.setCamera(new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 20, 10000));
         this.setScene(new THREE.Scene());
-        //this.scene().fog = new THREE.Fog( 0x000000, .1, 10000);
+        this.scene().fog = new THREE.Fog( 0x000000, .1, 5000);
         //this.scene().fog = new THREE.Fog( 0x000000, 0, 15000);
         //this.setControls(new THREE.OrbitControls(this.camera()));
         //this.controls().addEventListener("change", render );
@@ -117,9 +177,10 @@ class App extends BaseObject {
         this.scene().add(this.spotlight());    
         this.spotlight().target = this.mainObject()
 
-        this.setupTestSquare()
-        //this.setupTestObject()     
+        //this.setupTestSquare()
+        this.setupTestObject()     
         //this.loadModel("Hg_carrier")           
+        //this.loadModel("carrier")           
         //this.loadModel("Recognizer")
 
         this.setupFloor()
@@ -133,7 +194,7 @@ class App extends BaseObject {
         this.setRenderer(new THREE.WebGLRenderer());
         this.renderer().setClearColor(0x000000, 1.0);
         this.renderer().setSize(window.innerWidth, window.innerHeight);
-        this.renderer().shadowMapEnabled = true;
+        this.renderer().shadowMap.enabled = true;
         this.renderer().shadowMapSoft = true;
 
         this.setContainer(document.createElement("div"))
@@ -146,42 +207,52 @@ class App extends BaseObject {
         var geometry = new THREE.PlaneGeometry( 200, 200, 1, 1);
         var material = new THREE.MeshBasicMaterial( {color: 0x000000, side: THREE.DoubleSide} );
         var plane = new THREE.Mesh( geometry, material );
-        plane.addOutline()
-        this.mainObject().add( plane );
+        //plane.addOutline()
+        this.scene().add( plane );
     }
 
     setupTestObject() {
-        const geometry = new THREE.CubeGeometry(100,100,100);
+        const geometry = new THREE.CubeGeometry(10,10,10);
+        //geometry.normalize()
+        
         //var geometry = new THREE.IcosahedronGeometry(100, 0)
         //var geometry = new THREE.OctahedronGeometry(100, 0)
         //var geometry = new THREE.TetrahedronGeometry(100, 0)
         //var geometry = new THREE.DodecahedronGeometry(100, 0)
 
         const material = new THREE.MeshPhongMaterial( { 
-                ambient: 0x111111, 
                 color: 0xdddddd, 
                 specular: 0x333300, 
                 shininess: 30, 
-                shading: THREE.FlatShading 
+                flatShading: true
         }) 
-        
+
+        const group = new THREE.Object3D()
+        group.position.set(0,5,0)
+
         const object = new THREE.Mesh(geometry, material);
         object.position.set(0,0,0)
-        object.makeCastShadow()
-        object.recursiveSetColor(0x111111)
-        //object.recursiveSetColor(0x000000)
-        object.addOutline()
+        group.add( object );
 
-        this.mainObject().add( object );
+        const object2 = new THREE.Mesh(geometry, material);
+        object2.position.set(5,0,0)
+        group.add( object2 );
+
+        group.recursiveSetColor(0x111111)
+        group.recursiveSetOpacity(0)
+
+        group.addOutline()
+        this.scene().add( group );
     }
 
     setupFloor() {
-        this.setupFloorPlane()
+        //this.setupFloorPlane()
         this.setupFloorWire()
     }
 
     setupFloorPlane() {
-        const floorGeometry = new THREE.CubeGeometry(100000,.5,100000);
+        //const floorGeometry = new THREE.CubeGeometry(100000, .5, 100000, 10, 10);
+        const floorGeometry = new THREE.PlaneGeometry( 2000, 2000, 1, 1);
         //var floorMaterial = new THREE.MeshLambertMaterial({ color: 0x3d518b });
         //var floor = new THREE.Mesh(floorGeometry, floorMaterial);
         //var floorGeometry = new THREE.PlaneBufferGeometry( 20000, 20000, 30, 30 );
@@ -190,66 +261,81 @@ class App extends BaseObject {
             ambient: 0x0000ff, 
             wireframe: false, 
             //wireframeLinewidth: 4,
-            //fog:true,
+            //linewidth: 4,
+            fog:true,
             side: THREE.DoubleSide
         } );
         
         const floor = new THREE.Mesh(floorGeometry, material);
-        //floor.rotation.x = 3.14159/2
+        //floorWire.rotation.x = -Math.PI/2
+
+        //floor.rotation.x = Math.PI/2
         floor.position.x = 0;
         floor.position.y = 0;
         floor.position.z = 0;
         floor.receiveShadow = true;
         this.scene().add( floor );
-
     }
 
     setupFloorWire() {
-        const floorWireGeometry = new THREE.PlaneGeometry( 20000, 20000, 30, 30 );
+        const floorWireGeometry = new THREE.PlaneGeometry( 10000, 10000, 30, 30);
+        
+        const material2 = new THREE.LineBasicMaterial( 
+            {
+                color: 0xffffff, 
+                linewidth: 1,
+                fog:true,
+                //transparent: true,
+                //opacity:1,
+            }
+        );
+
         const material = new THREE.MeshLambertMaterial( {
             color: 0xffffff, 
-            ambient: 0x0000ff, 
             wireframe: true, 
-            wireframeLinewidth: 1,
-            fog:true,
-            //transparent: true,
-            //opacity:.5,
-            side: THREE.DoubleSide
-            } );
-        const floorWire = new THREE.Mesh( floorWireGeometry, material );
-        //floorWire.rotation.z += -3.14159/4
-        floorWire.rotation.x = -3.14159/2
-        floorWire.position.y += 1
-        //floorWire.receiveShadow = true;
+            wireframeLinewidth: 10,
+            fog: true,
+        } );
+    
+        const floorWire = new THREE.Mesh( floorWireGeometry, material);
+        //floorWire.rotation.z += -Math.PI/4
+        floorWire.rotation.x = -Math.PI/2
+        floorWire.position.y += 0.00
+        floorWire.receiveShadow = true;
         this.scene().add( floorWire );
     }
 
     loadModel(modelName) {
-        console.log("loading...")
-        const loader = new THREE.OBJMTLLoader();
+        console.log("loading '" + modelName + "'...")
+        //const loader = new THREE.OBJMTLLoader();
+        const loader = new THREE.OBJLoader();
         //const loader = new THREE.ObjectLoader();
-        loader.load("models/" + modelName + ".obj", "models/" + modelName +".mtl");    
-        loader.addEventListener("load", (event) => { this.didFinishLoadModel(event) } );
+        const callback = (event) => { this.didFinishLoadModel(event, modelName) };
+        //loader.load("models/" + modelName + ".obj", "models/" + modelName +".mtl", callback, (event) => { this.didFinishLoadModel(event) });    
+        loader.load("models/" + modelName + ".obj", callback);    
+        //loader.addEventListener("load", callback );
     }
 
-    didFinishLoadModel(event) {
-        console.log("loaded")
+    didFinishLoadModel(object, modelName) {
+        console.log("loaded '" + modelName + "'")
 
-        const object = event.content
+        //object.moveToOrigin()
+        //object.scaleToUnit()
+        
         //object.makeCastShadow()
         //object.makeDoubleSided()
         //object.recursiveSetColor(0x004400)
-        //object.recursiveSetColor(0x552222)
-        object.recursiveSetColor(0x000000)
+        object.recursiveSetColor(0x222222)
+        //object.recursiveSetColor(0x000000)
         object.makeCastShadow()
-
-        //const group = new THREE.Object3D();
-        //group.add(object)
         
-        object.addOutline(0xff6600, 3, 1)
+        object.addOutline(0xff6600, 6, 0.5)
         //object.addOutline(0xff6600, 10, .5) // glow
 
-        this.mainObject().add(object);
+        const outline = object.asEdgesObject(0xff6600, 10, .5)
+        this.mainObject().add(outline);
+
+        //this.mainObject().add(object);
         
         const ms = 100 / this.mainObject().radius()
         this.mainObject().scale.set(ms, ms, ms)
@@ -259,36 +345,38 @@ class App extends BaseObject {
 
     updateCamera() {
         const speed = 1 / 500
-        //this.camera().position.y += .01
-        const r = this.mainObject().radius() * 15
-        this.camera().position.x = r * Math.cos(speed * this.time())
-        this.camera().position.y = this.mainObject().radius() * 20 + r * Math.cos(speed * this.time() * 1.5)
-        this.camera().position.z = r * Math.sin(speed * this.time())
         
+        const mainRadius = 100 //this.mainObject().radius()/4
+        const r = mainRadius 
+        const t = this.time()
+        
+        // the x-z horizon plane
+        const f = 4.5
+        this.camera().position.x = f * r * Math.cos(speed * t)
+        this.camera().position.z = f * r * Math.sin(speed * t)
+        
+        // y is height
+        this.camera().position.y = 1*r + 3 * r * Math.cos(speed * t * 0.5)
+       //this.camera().position.y = 3*r
+
+        /*
+        const ymin = 3
+        if (this.camera().position.y < ymin) {
+            this.camera().position.y = ymin
+        }
+        */
         //this.mainObject().rotation.y += .05
         
         const p = this.mainObject().position.clone()
-        //p.y *= 6
+        p.y *= 1
+        //const p = this.mainObject().centerPoint()
         this.camera().lookAt(p)
     }
 
     render() {
-        if (this.mainObject()) {
-            this.setTime(this.time() + 6)
+        this.setTime(this.time() + 2)
 
-            this.updateCamera()
-
-            if (this.mainObject().step) {
-                this.mainObject().step()
-                /*
-                this.mainObject().step = function() {
-                    this.rotation.x += .005
-                    this.rotation.y += .005   
-                }
-                */
-            }
-        }
-
+        this.updateCamera()
         this.renderer().render(this.scene(), this.camera());
     }
 
@@ -302,8 +390,10 @@ class App extends BaseObject {
         const f = 1
         this.camera().aspect = window.innerWidth / window.innerHeight;
         this.camera().updateProjectionMatrix();
-        const w = Math.floor(window.innerWidth / f);
-        const h = Math.floor(window.innerHeight / f);
+        //const w = Math.floor(window.innerWidth / f);
+        //const h = Math.floor(window.innerHeight / f);
+        const w = window.innerWidth  / f;
+        const h = window.innerHeight / f;
         this.renderer().setSize(w, h);
     }
 
@@ -312,9 +402,9 @@ class App extends BaseObject {
     }
 }
 
-app = App.clone()
 
 window.onload = function() {
+    app = App.clone()
     app.run()
 }
 
